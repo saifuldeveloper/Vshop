8268931277a864d35e9cdec8abd2de9d91faaaa4
saifuldeveloper <mdsaiful77336@gmail.com>
2025-04-24 15:45:22 +0600

    upload

---
+++ app/Http/Middleware/redirectAdminMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class redirectAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next ,$guard = null): Response
+    {
+       
+        if(Auth::guard($guard)->check() && Auth::user()->is_admin == 1){
+            return redirect()->route('admin.dashboard');
+        }
+
+        return $next($request);
+
+    }
+}
---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted } from 'vue'
+import Navbar from './Navbar.vue'
+import Sidebar from './Sidebar.vue'
+
+</script>
+
+
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        <!---Navbar Start-->
+        <Navbar />
+        <!--Navbar End-->
+
+        <!-- Sidebar  start-->
+
+        <sidebar />
+        <!----Sidebar End-->
+
+        <!----main Section Start-->
+
+        <slot />
+
+        <!-- Main Section End -->
+    </div>
+</template>
